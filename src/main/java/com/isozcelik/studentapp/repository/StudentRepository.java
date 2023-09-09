2c97ea774f88e32bb8f5f2504d20ca50fe9464a5
ismail h. ozcelik <iozcelyk@gmail.com>
2023-09-09 23:18:57 +0300

    repository

--- src/main/java/com/isozcelik/studentapp/repository/StudentRepository.java
+++ src/main/java/com/isozcelik/studentapp/repository/StudentRepository.java
@@ -1,6 +1,12 @@
 package com.isozcelik.studentapp.repository;
 
-public interface StudentRepository {
+import com.isozcelik.studentapp.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 
 
